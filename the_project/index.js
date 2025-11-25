1a1eaaf1ec7f996da818c6d92302c68e0af2687d
Miska Malkamaki <mitamalk.jyu.fi>
2025-11-25 16:30:53 +0200

    simple webpage

--- the_project/index.js
+++ the_project/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'index.html'))
+})
 
 
 const PORT = process.env.PORT || 3001
